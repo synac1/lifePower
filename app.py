bb521a12b337d6d6ff6e37a614b254475aed351f
Yanilda <yanilda.peralta@gmail.com>
2018-04-25 19:55:42 -0400

    modified:   app.py 	new file:   helpers/__init__.py 	new file:   static/ico/favicon.ico 	new file:   templates/request.html

--- app.py
+++ app.py
@@ -1,4 +1,45 @@
 ##
-# file name app.py
-#
+# file name: app.py
+# author: Yanilda Peralta
 ##
+import os
+from flask import Flask, render_template, send_from_directory
+
+
+#----------------------------------------
+# initialization
+#----------------------------------------
+
+app = Flask(__name__)
+
+app.config.update(
+    DEBUG = True,
+)
+
+#----------------------------------------
+# controllers
+#----------------------------------------
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "404 page not found"
+
+@app.route("/")
+def index():
+    return "Welcome home"
+#----------------------------------------
+# launch
+#----------------------------------------
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port)
+
+    print("running")
+    
+
+
